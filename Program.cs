026b371519a744b03985de20f4895ff89c29632c
nicolaeblaga <nicolae.blagaa@gmail.com>
2015-01-20 22:19:15 +0000

    hello
    hello message

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            String name = "";
+            Console.Write("Enter your name: ");
+            name = Convert.ToString(Console.ReadLine());
+
+            Console.WriteLine("Hello " + name);
+            Console.ReadLine();
+        }
+    }
+}
